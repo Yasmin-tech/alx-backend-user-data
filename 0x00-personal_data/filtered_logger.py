557adbba16cfd73bddcf1731e9e9bde2b155cb01
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-13 01:45:57 +0300

    add task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@
     """
 
 
+import logging
 import re
 from typing import List
 
@@ -24,3 +25,28 @@ def filter_datum(
                 message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+            format the message
+            """
+        message = filter_datum(
+                self.fields,
+                self.REDACTION,
+                record.msg,
+                self.SEPARATOR)
+        record.msg = message  # Update the message in the record
+        return super(RedactingFormatter, self).format(record)
