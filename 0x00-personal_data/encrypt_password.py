c3be02b45d27ef05c86dcfe4b6cff74eaeddcfca
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-13 03:35:35 +0300

    add task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,11 @@ def hash_password(password: str) -> bytes:
     """Hashes a password using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Use bcrypt to validate that the provided password
+        matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
