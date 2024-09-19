db5fabf59c1bc090f04fad2c8adce9cd10cba452
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-19 16:30:46 +0300

    add task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Create a class that inherits from Auth
+    and implement a SessionAuth mechanism
+    """
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        implement a SessionAuth mechanism
+        """
+    pass
