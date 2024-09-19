ee40ed35753c7df4db929556715aa4979d96fabf
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-19 16:46:53 +0300

    add task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,10 +6,23 @@
 
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
         implement a SessionAuth mechanism
         """
-    pass
+    # a dictionary where key represent session_id and value is the user_id
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            generate a random session id and add it to the dictionary
+            <user_id_by_session_id>
+            """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
