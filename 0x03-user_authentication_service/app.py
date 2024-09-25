de4cfa565d6aaabcdde85ace95c40a269f1d51ac
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-25 14:06:50 +0300

    task 11 again

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -42,9 +42,9 @@ def login() -> str:
         - The account login payload.
     """
     email, password = request.form.get("email"), request.form.get("password")
-    if not AUTH.valid_login(email, password):
+    if not auth.valid_login(email, password):
         abort(401)
-    session_id = AUTH.create_session(email)
+    session_id = auth.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
