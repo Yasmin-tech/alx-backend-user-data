a9ea64a603b5053c58f801770086fc34d414a11f
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-25 13:10:27 +0300

    add task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,3 +45,26 @@ class Auth:
         hashed_pass = _hash_password(password)
         new_user = self._db.add_user(email, hashed_pass)
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            Locate the user by email, if it exists and was registered with
+            the given password, return True.
+            Otherwise, return False
+            """
+        if not email or not isinstance(email, str):
+            return False
+        if not password or not isinstance(password, str):
+            return False
+        user_obj = None
+
+        try:
+            user_obj = self._db.find_user_by(email=email)
+        except NoResultFound:
+            pass
+
+        if user_obj is None:
+            return False
+        if bcrypt.checkpw(password.encode(), user_obj.hashed_password):
+            return True
+        return False
