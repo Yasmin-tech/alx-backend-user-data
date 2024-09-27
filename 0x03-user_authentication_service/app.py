9f88465efa2a84b4a563ecc1452af582f84821ea
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-09-27 13:23:56 +0300

    add task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,5 +88,18 @@ def profile() -> str:
     return jsonify({"email": user_instance.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+        Get a reset_password_token if user with the given email exists
+        """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
